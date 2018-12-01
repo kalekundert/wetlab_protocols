db8e36e43cb50ebaad31d4b338e534ddc8757e32
Kale Kundert <kale@thekunderts.net>
2018-11-30 20:55:50 -0800

    Add ezDNase protocol.

--- reverse_transcribe.py
+++ reverse_transcribe.py
@@ -4,11 +4,15 @@
 Reverse-transcribe RNA to cDNA.
 
 Usage:
-    reverse_transcribe.py [<rna_conc.tsv>]
+    reverse_transcribe.py [<rna_conc.tsv>] [options]
 
 Arguments:
     <rna_conc.tsv>
         Concentrations of the RNAs, as exported from the Nanodrop.
+
+Options:
+    -d --dnase
+        Add a DNase pre-treatment step to the beginning of the protocol.
 """
 
 import docopt
@@ -34,25 +38,51 @@ Construct                   (ng/uL)     (µL)       (µL)
         name = row['Sample Name']
         conc =  row['conc']
         vol = 1000 / conc
-        concs += f"{name:25s}    {conc:6.2f}   {vol:6.2f}    {8-vol:6.2f}\n"
+        ref = 8 if not args['--dnase'] else 4
+        concs += f"{name:25s}    {conc:6.2f}   {vol:6.2f}    {ref-vol:6.2f}\n"
     concs += """\
 ───────────────────────────────────────────────────────
 """
 
-protocol += f"""\
+if args['--dnase']:
+    protocol += f"""\
+Setup DNase reactions for each sample:
+
+- 1 µL 10 ezDNase buffer
+- 1 µL ezDNase enzyme (Invitrogen 11766051)
+- 1 µg RNA
+- Water to 6 µL
+
+Consider preparing a 2x ezDNase master mix.
+"""
+
+    protocol += """\
+Incubate at 37°C for 2 min.  Then briefly 
+centrifuge and place on ice.
+"""
+
+    protocol += """\
+Add 2 µL SuperScript IV VILO master mix 
+(Invitrogen 11766050) and 2 µL water to each 
+sample.
+
+The master mix contains random hexamers, which 
+will prime the reverse transcription of all RNAs 
+in the sample.
+"""
+else:
+    protocol += f"""\
 Setup reverse transcription reactions for each 
 sample:
 
 - 1 µg RNA
-- 2 µL SuperScript VILO master mix (Invitrogen 
-  11755050)
+- 2 µL SuperScript IV VILO master mix
+  (Invitrogen 11766050)
 - water to 10 µL
 
 The master mix contains random hexamers, which 
 will prime the reverse transcription of all RNAs 
 in the sample.
-
-{concs}
 """
 
 protocol += """\
@@ -66,4 +96,7 @@ Incubate at the following temperatures:
 
 if __name__ == '__main__':
     print(protocol)
+    if args['<rna_conc.tsv>']:
+        print()
+        print(concs)
 
