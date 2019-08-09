e1ada9f4a71bc13d170222e1fca4e19ba7b0e4ee
Kale Kundert <kale@thekunderts.net>
2019-08-09 14:10:37 -0400

    Require template and primer names.

--- pcr.py
+++ pcr.py
@@ -2,15 +2,26 @@
 
 """\
 Usage:
-    pcr.py <num_reactions> <annealing_temp> [options]
+    pcr.py <template> <fwd_primer> <rev_primer>
+           <num_reactions> <annealing_temp> <extension_time> [options]
 
 Arguments:
+    <template>
+        The name of the template.
+
+    <primers>
+        The name of the primers.
+
     <num_reactions>
         The number of reactions to set up.
 
     <annealing_temp>
         The annealing temperature for the PCR reaction (in °C).  I typically 
         use NEB's online "Tm Calculator" to determine this parameter.
+
+    <extension_time>
+        The length of the extension step in seconds.  The rule of thumb is 30 
+        sec/kb, perhaps longer if you're amplifying a whole plasmid.
 
 Options:
     -v --reaction-volume <μL>       [default: 10]
@@ -37,10 +48,6 @@ Options:
         Indicate which additives should be included in the reaction.  Valid
         additives are 'dmso' and 'betaine'.
 
-    -x --extension-time <secs>      [default: 120]
-        The length of the annealing step in seconds.  The rule of thumb is 30 
-        sec/kb, perhaps longer if you're amplifying a whole plasmid.
-
     -P --no-primer-mix
         Don't show how to prepare the 10x primer mix.
 
@@ -56,11 +63,15 @@ import docopt
 import dirty_water
 
 args = docopt.docopt(__doc__)
-
-pcr = dirty_water.Pcr(polymerase=args['--polymerase'])
+pcr = dirty_water.Pcr(
+        template=args['<template>'],
+        fwd_primer=args['<fwd_primer>'],
+        rev_primer=args['<rev_primer>'],
+        polymerase=args['--polymerase'],
+)
 pcr.num_reactions = eval(args['<num_reactions>'])
 pcr.annealing_temp = args['<annealing_temp>']
-pcr.extension_time = int(eval(args['--extension-time']))
+pcr.extension_time = int(eval(args['<extension_time>']))
 pcr.dmso = 'dmso' in args['--additives']
 pcr.betaine = 'betaine' in args['--additives']
 pcr.template_in_master_mix = 'dna' in args['--master-mix'] and not args['--nothing-in-master-mix']
@@ -68,9 +79,9 @@ pcr.primers_in_master_mix = 'primers' in args['--master-mix'] and not args['--no
 pcr.additives_in_master_mix = 'additives' in args['--master-mix'] and not args['--nothing-in-master-mix']
 pcr.make_primer_mix = not args['--no-primer-mix']
 pcr.reaction.volume = float(args['--reaction-volume'])
-pcr.reaction['template DNA'].stock_conc = float(args['--dna-stock-conc'])
+pcr.reaction[args['<template>']].stock_conc = float(args['--dna-stock-conc'])
 
 if args['--dna-final-conc']:
-    pcr.reaction['template DNA'].conc = float(args['--dna-final-conc'])
+    pcr.reaction[args['<template>']].conc = float(args['--dna-final-conc'])
 
 print(pcr)
