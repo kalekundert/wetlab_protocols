53b527bce3439dcd11019b4c6ebd11bc30e55953
Kale Kundert <kale@thekunderts.net>
2019-08-09 14:21:59 -0400

    Update pcr_cloning.py CLI to match pcr.py

--- pcr_cloning.py
+++ pcr_cloning.py
@@ -6,10 +6,16 @@ Make a small modification to a plasmid by amplifying it with primers that have
 overhangs with your desired change.
 
 Usage:
-    pcr_cloning.py <num_reactions> <annealing_temp> [options]
-    pcr_cloning.py <num_reactions> -L [options]
+    pcr_cloning.py <template> <fwd_primer> <rev_primer>
+           <num_reactions> <annealing_temp> <extension_time> [options]
 
 Arguments:
+    <template>
+        The name of the template.
+
+    <primers>
+        The name of the primers.
+
     <num_reactions>
         The number of reactions to set up.
 
@@ -17,11 +23,11 @@ Arguments:
         The annealing temperature for the PCR reaction (in °C).  I typically 
         use NEB's online "Tm Calculator" to determine this parameter.
 
-Options:
-    -x --extension-time <secs>      [default: 120]
-        The length of the annealing step in seconds.  The rule of thumb is 30 
+    <extension_time>
+        The length of the extension step in seconds.  The rule of thumb is 30 
         sec/kb, perhaps longer if you're amplifying a whole plasmid.
 
+Options:
     -v --reaction-volume <μL>       [default: 10]
         The volume of the PCR reaction.  The recommended volumes for Q5 are 25
         and 50 μL.
@@ -33,6 +39,14 @@ Options:
     -M --nothing-in-master-mix
         Don't include anything but water and polymerase in the master mix.  
         This is an alias for: -m ''
+
+    -p --polymerase <name>  [default: q5]
+        The name of the polymerase being used.  Different polymerases also have 
+        different thermocycler parameters, as recommended by the manufacturer.  
+        Currently, the following polymerases are supported:
+
+        q5: Q5 High-Fidelity DNA Polymerase (NEB)
+        ssoadv: SsoAdvanced™ Universal SYBR® Green Supermix (Biorad)
 
     -a --additives <dmso,betaine>   [default: '']
         Indicate which additives should be included in the reaction.  Valid
@@ -60,10 +74,15 @@ protocol = dirty_water.Protocol()
 ## PCR
 
 if not args['--skip-pcr']:
-    pcr = dirty_water.Pcr()
+    pcr = dirty_water.Pcr(
+            template=args['<template>'],
+            fwd_primer=args['<fwd_primer>'],
+            rev_primer=args['<rev_primer>'],
+            polymerase=args['--polymerase'],
+    )
     pcr.num_reactions = eval(args['--num-pcr']) if args['--num-pcr'] else eval(args['<num_reactions>'])
-    pcr.annealing_temp = int(args['<annealing_temp>'])
-    pcr.extension_time = int(args['--extension-time'])
+    pcr.annealing_temp = args['<annealing_temp>']
+    pcr.extension_time = int(eval(args['<extension_time>']))
     pcr.dmso = 'dmso' in args['--additives']
     pcr.betaine = 'betaine' in args['--additives']
     pcr.template_in_master_mix = 'dna' in args['--master-mix'] and not args['--nothing-in-master-mix']
@@ -72,8 +91,7 @@ if not args['--skip-pcr']:
     pcr.make_primer_mix = not args['--no-primer-mix']
     pcr.reaction.volume = float(args['--reaction-volume'])
 
-    if pcr.num_reactions > 0:
-        protocol += pcr
+    protocol += pcr
 
 ## Ligation
 
