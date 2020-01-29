ae20c800ce2dd0404ad62afb3b24397abfc24449
Kale Kundert <kale@thekunderts.net>
2020-01-29 09:54:04 -0800

    Use stepwise formatting.

--- serial_dilution.py
+++ serial_dilution.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 """\
+Perform a serial dilution.
+
 Usage:
     serial_dilution.py <volume> <high> <low> <steps> [options]
-
-Options:
-    -v --verbose
-        Print out the concentration for each step of the dilution.
 """
 
 if __name__ == '__main__':
     import docopt
+    import stepwise
+    from tabulate import tabulate
+
     args = docopt.docopt(__doc__)
     volume = eval(args['<volume>'])
     high = eval(args['<high>'])
@@ -21,19 +22,22 @@ if __name__ == '__main__':
     transfer = volume * dilution / (1 - dilution)
     initial_volume = volume + transfer
 
-    print("""\
-1. Put {initial_volume:.2f} μL material in the first tube.
-2. Add {volume:.2f} μL water in the remaining tubes.
-3. Perform a serial dilution, transferring {transfer:.2f} μL
-   each time.
-""".format(**locals()))
-    
-    if args['--verbose']:
-        from tabulate import tabulate
-        print('Final concentrations:')
-        print(tabulate([
-            [i+1, high * dilution**i]
-            for i in range(steps)
-        ], tablefmt='plain', floatfmt='.2e'))
+    protocol = stepwise.Protocol()
+    protocol += f"""\
+Perform a serial dilution [1]:
+
+- Put {initial_volume:.2f} μL material in the first tube.
+- Add {volume:.2f} μL water in the remaining tubes.
+- Transfer {transfer:.2f} μL between each tube.
+"""
+    protocol.footnotes[1] = f"""\
+The final concentrations will be:
+{tabulate(
+        [[i+1, high * dilution**i] for i in range(steps)],
+        tablefmt='plain',
+        floatfmt='.2e',
+)}
+"""
+    print(protocol)
 
 # vim: tw=53
